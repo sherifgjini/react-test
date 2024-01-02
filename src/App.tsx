d952858fd3c1058c25dcf15b80efc5340504748e
Sherif <sherif@gjirafa.com>
2024-01-02 02:20:42 +0100

    Project

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import "./App.css";
+import { CounterTwo } from "./components/counter-two/CounterTwo";
 import { MuiMode } from "./components/mui/MuiMode";
+import { useCounter } from "./hooks/use-counter/useCounter";
 import { AppProviders } from "./providers/AppProviders";
 
 function App() {
+  const {increment} = useCounter();
   return (
     <div className="App">
       <AppProviders>
         {/* <Counter /> */}
+        <CounterTwo count={1} handleIncrement={() => increment} />
         <MuiMode />
       </AppProviders>
     </div>
---
+++ src/components/counter-two/CounterTwo.test.tsx
@@ -0,0 +1,43 @@
+import { render, screen } from "@testing-library/react";
+import { CounterTwo } from "./CounterTwo";
+import user from "@testing-library/user-event"
+
+describe("CounterTwo", () => {
+  test("Renders correctly", () => {
+    render(<CounterTwo count={1} />);
+    const heading1 = screen.getByRole("heading");
+    expect(heading1).toBeInTheDocument();
+  });
+  test("Does not Render buttons at the beggining", () => {
+    render(<CounterTwo count={1} />);
+    const incrementBtn = screen.queryByRole("button", { name: "Increment" });
+    expect(incrementBtn).not.toBeInTheDocument();
+  });
+//   test("Renders conditionaly increment button", async () => {
+//     const incrementHandler = jest.fn();
+//     render(<CounterTwo count={1} handleIncrement={incrementHandler} />);
+//     const incrementBtn = await screen.findByRole("button", {
+//       name: "Increment",
+//     });
+//     expect(incrementBtn).toBeInTheDocument();
+//   });
+  test("Functions are rendering correctly", async () => {
+    user.setup();
+    const incrementHandler = jest.fn();
+    const decrementHandler = jest.fn();
+    render(
+      <CounterTwo
+        count={1}
+        handleIncrement={incrementHandler}
+        handleDecrement={decrementHandler}
+      />
+    );
+    const handleIncrement = screen.getByRole("button", { name: "Increment" });
+    const handleDecrement = screen.getByRole("button", { name: "Decrement" });
+    await user.click(handleIncrement);
+    await user.click(handleDecrement);
+    expect(incrementHandler).toHaveBeenCalledTimes(1)
+    expect(decrementHandler).toHaveBeenCalledTimes(1)
+    
+  });
+});
